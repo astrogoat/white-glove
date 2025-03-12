8c8eb102e22c76560e850c333403bfab5334b72d
OniiCode <oniicoder@gmail.com>
2025-03-12 08:48:13 +0000

    Add settings to migration

--- database/migrations/settings/2025_03_10_162142_create_white-glove_settings.php
+++ database/migrations/settings/2025_03_10_162142_create_white-glove_settings.php
@@ -7,10 +7,18 @@ return new class extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('white-glove.enabled', false);
+        $this->migrator->add('white-glove.icon', '');
+        $this->migrator->add('white-glove.title', '');
+        $this->migrator->add('white-glove.details', '');
+        $this->migrator->add('white-glove.modal', '');
     }
 
     public function down()
     {
         $this->migrator->delete('white-glove.enabled');
+        $this->migrator->delete('white-glove.icon');
+        $this->migrator->delete('white-glove.title');
+        $this->migrator->delete('white-glove.details');
+        $this->migrator->delete('white-glove.modal');
     }
 };
